f563daa659ef9238f28ecdf68126c48d16cea135
AmazonProject_pakji <phichak.limlamlertkul@gmail.com>
2025-12-05 23:39:14 +0700

    Use products.js file

--- script/amazon.js
+++ script/amazon.js
@@ -1,31 +1,5 @@
-const products = [{
-    image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating : {
-        star : 4.5,
-        count : 87
-    },
-    priceCents : 1090 // 1 dollar = 100 cents
-}, {
-    image : 'images/products/intermediate-composite-basketball.jpg',
-    name : 'Intermediate Size Basketball',
-    rating : {
-        star : 4,
-        count : 127
-    },
-    priceCents : 2095
-} , {
-    image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating : {
-        star : 4.5,
-        count : 56
-    },
-    priceCents : 799
-}];
-
 let productsHTML = '';
-
+// products eill be coming from data/products.js file
 products.forEach((product) => {
     productsHTML += `
     <div class="product-container">
@@ -47,7 +21,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)} // .toFixed(2) shoe number with 2 decimal
+        $${(product.priceCents / 100).toFixed(2)}
         </div>
 
         <div class="product-quantity-container">
